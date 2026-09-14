050f64304bb0f33d958bf3f8681c2764654abeb7
admiraltkirk-ai <admiraltkirk@gmail.com>
2026-09-14 13:43:50 +0100

    Wait for Office.js before registering handler
    Wait for Office.js before registering handler

--- launchevent.js
+++ launchevent.js
@@ -2,4 +2,11 @@ function onMessageSendHandler(event) {
   event.completed({ allowEvent: true });
 }
 
-Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
+Office.onReady(function () {
+  console.log("Office.js ready - registering Smart Alert handler");
+
+  Office.actions.associate(
+    "onMessageSendHandler",
+    onMessageSendHandler
+  );
+});
