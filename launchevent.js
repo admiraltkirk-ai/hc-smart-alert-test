e2ea7f4dae08af00c92f23ef939d1e24abef3653
admiraltkirk-ai <admiraltkirk@gmail.com>
2026-09-14 13:48:55 +0100

    Use Microsoft Smart Alerts handler pattern
    Use Microsoft Smart Alerts handler pattern

--- launchevent.js
+++ launchevent.js
@@ -2,11 +2,7 @@ function onMessageSendHandler(event) {
   event.completed({ allowEvent: true });
 }
 
-Office.onReady(function () {
-  console.log("Office.js ready - registering Smart Alert handler");
-
-  Office.actions.associate(
-    "onMessageSendHandler",
-    onMessageSendHandler
-  );
-});
+Office.actions.associate(
+  "onMessageSendHandler",
+  onMessageSendHandler
+);
