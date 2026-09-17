5163d851402ca4425148fe37bc7883bfa328a4d5
admiraltkirk-ai <admiraltkirk@gmail.com>
2026-09-17 15:55:03 +0100

    Fix Smart Alert Office.js initialization
    Fix Smart Alert Office.js initialization

--- launchevent.js
+++ launchevent.js
@@ -2,11 +2,16 @@ console.log("HC Smart Alert: launchevent.js loaded");
 
 function onMessageSendHandler(event) {
   console.log("HC Smart Alert: onMessageSendHandler fired");
-
   event.completed({ allowEvent: true });
 }
 
-Office.actions.associate(
-  "onMessageSendHandler",
-  onMessageSendHandler
-);
+Office.onReady(function () {
+  console.log("HC Smart Alert: Office.js ready");
+
+  Office.actions.associate(
+    "onMessageSendHandler",
+    onMessageSendHandler
+  );
+
+  console.log("HC Smart Alert: handler associated");
+});
